a4e44a589afd76136a5f132fe77bd2f0d4809f72
aakashindoriya <aakashindoriya12346@gmail.com>
2024-10-12 18:42:39 +0530

    server is ready with db connection

---
+++ server/index.js
@@ -0,0 +1,19 @@
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const connect = require("./config/connect")
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get("/", (req, res) => res.send("welcome to CIMET"))
+
+
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, async () => {
+    await connect()
+    console.log(`listenning on Port :${PORT}`)
+})
